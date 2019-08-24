a7a7e3cfc6d51a9d6e8534bb1fe09663a51f1424
Marcelo Santos <rocha.mer21@gmail.com>
2019-08-23 22:16:13 -0300

    terminado - adicionar, remover e editar os gêneros

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from './Header'
+import Generos from './Generos'
+import NovoGenero from './NovoGenero'
+import axios from 'axios'
+import EditarGenero from './EditarGenero'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const Home = () => {
   return <h1>Home</h1>
 }
-const Generos = () => {
-  return <h1>Generos</h1>
-}
 
 function App() {
+  const [data, setData] = useState({})
+  useEffect(() => {
+    axios.get('/api').then(res => {
+      setData(res.data)
+    })
+  }, [])
+
   return (
     <Router>
       <div>
         <Header />
         <Route path='/' exact component={Home} />
-        <Route path='/generos' component={Generos} />
+        <Route path='/generos/:id' exact component={EditarGenero} />        
+        <Route path='/generos/novo' exact component={NovoGenero} />
+        <Route path='/generos' exact component={Generos} />
+        <pre>{JSON.stringify(data)}</pre>
       </div>
     </Router>
   );
